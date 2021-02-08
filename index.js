0888040aa39a87e6c8123de583a99b5a8e28de89
Tri <triphan921@gmail.com>
2021-02-08 13:23:51 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+
+function writeCards(arr, str) {
+    return arr.map(x=> `Thank you, ${x}, for the wonderful ${str} gift!`);
+}
+
+function countDown(n) {
+    var i = n;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
